315cd00fbc379df37abbd4c8744b94d1e9491eb4
Igor Ignatev <1761812+brainfair@users.noreply.github.com>
2023-03-14 21:05:54 +0200

    add templates

--- kubelinks_app/home/routes.py
+++ kubelinks_app/home/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-from . import gateway_list, ingress_list
+from . import gateway_list, ingress_list, extraurls_list
 
 home_bp = Blueprint('home_bp', __name__,
                     template_folder='templates')
@@ -10,11 +10,13 @@ home_bp = Blueprint('home_bp', __name__,
 @home_bp.route('/', methods=["GET"])
 def home():
     try:
-        ingress = ingress_list.get_ingress_list()
-        gateway = gateway_list.get_gw_list()
+        ingress   = ingress_list.get_ingress_list()
+        gateway   = gateway_list.get_gw_list()
+        extraurls = extraurls_list.get_eu_list()
 
         return render_template("home.html",
                                ingress=ingress,
-                               gateway=gateway)
+                               gateway=gateway,
+                               extraurls=extraurls)
     except TemplateNotFound:
         abort(404)
