d9c3387d5c995f344f5371d630624bd1cdb9eacb
kkirara <kirara@bk.ru>
2024-01-09 15:42:35 +0200

    add flask healthz

--- kubelinks_app/__init__.py
+++ kubelinks_app/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 import os
 from kubernetes import config
 from kubelinks_app.home.routes import home_bp
+from flask_healthz import healthz
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 
 app = Flask(__name__)
-app.register_blueprint(home_bp)
-LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 app.logger.setLevel(LOGLEVEL)
+app.config["HEALTHZ"] = {
+    "live": lambda: None,
+    "ready": lambda: None,
+}
+app.register_blueprint(healthz, url_prefix="/healthz")
+app.register_blueprint(home_bp)
 
 error = None
-
 try:
     config.load_config()
 except Exception as e:
