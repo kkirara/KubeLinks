5f5203d1fe123d48b08728353d8880f3cfcae83d
kkirara <kirara@bk.ru>
2024-03-01 17:40:21 +0200

    to change static files url if has base_path

--- kubelinks_app/__init__.py
+++ kubelinks_app/__init__.py
@@ -8,7 +8,7 @@ import kubelinks_app.config as config
 
 def create_app(config=config.ProdactionConfig):
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path=f'{config.BASE_PATH}/static')
     app.config.from_object(config)
 
     app.register_blueprint(home_bp, url_prefix=config.BASE_PATH)
